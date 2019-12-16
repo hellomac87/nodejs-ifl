8dfdc01bcca7097a9c521ea11ba6bd71efed45c9
이강산 <mountain@igangsan-ui-MacBookPro.local>
2019-12-16 22:41:52 +0900

    static 디렉토리 설정

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var app = express();
 app.listen(3000, function(){
     console.log('start! express server on port 3000');
 });
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/public/main.html")
+})
+
+app.get('/main', function(req, res){
+    res.sendFile(__dirname + "/public/main.html")
+})
