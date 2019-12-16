b86b2e1eede52fad810b36f7862c4a350fdb4a66
이강산 <mountain@igangsan-ui-MacBookPro.local>
2019-12-16 22:58:20 +0900

    POST 요청처리

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 app.listen(3000, function(){
     console.log('start! express server on port 3000');
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/public/main.html")
@@ -12,4 +16,9 @@ app.get('/', function(req, res){
 
 app.get('/main', function(req, res){
     res.sendFile(__dirname + "/public/main.html")
+})
+
+app.post('/email_post', function(req, res){
+    console.log(req.body.email);
+    res.send("<h1>welcome! " + req.body.email + "</h1>");
 })
